8fe543695e0772085e1576727a157edee153b1d8
IwM341 <artem_tovstun@mail.ru>
2023-02-07 14:00:13 +0300

    all

--- tests/try_test/test_masses.py
+++ tests/try_test/test_masses.py
@@ -1,9 +1,28 @@
 import test_functions as tf
 import pandas as pd
-masses = [10,30,50,70,100]
+
+'''python script for testing programm'''
+'''masses - array of masses being tested'''
+mk = [10,30,50,70,100]
+'''dmk - delta mass'''
 dmk = 0.0
-#tr = tf.test_parametr_array_mks(masses,dmk,parallel = False)
-tr = tf.load_list_mk(masses,dmk)
+
+'''remark: to test dmk with fixed mass, 
+        just swap types (i.e. write mk =  mass, dmk = [list of dmk])'''
+
+
+'''tf.test_parametr_array_mks runs programm, 
+to specify path to programm, write in args prog_name = path/to/prog.
+to test dmk use function test_parametr_array_dmks'''
+#tr = tf.test_parametr_array_mks(mk,dmk,parallel = False)
+
+'''tf.load_list_mk loads all fraction files. Use it if programm has worked and previous line is commented.
+if files are not ready, uncomment previous line and comment this line'''
+tr = tf.load_list_mk(mk,dmk)
+
+'''save results to datatable'''
 df = pd.DataFrame([ tf.dict_concat(el['H'],{'mk':el['mk'],'dmk':el['dmk']}) for el in tr])
 df.to_csv('df.txt', header=None, index=None, sep = '\t')
+
+'''this line prints gnuplot command to plot distributions'''
 print(tf.plotting_command(df.columns,'df.txt','mk'))
